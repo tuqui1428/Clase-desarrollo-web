510cb3130d85e05cb4428145a70134a369bfec84
tuqui1428 <119327150+tuqui1428@users.noreply.github.com>
2022-11-28 09:03:06 -0500

    Add files via upload

---
+++ carlos123/src/PrimerClase.java
@@ -0,0 +1,34 @@
+public class PrimerClase {
+    public static void main ( String[] args) {
+
+        String nombre = "Carlos";
+        String apellido = "Garcia";
+
+        int edad= 50;
+        System.out.println ("hola " +  nombre + " " + apellido);
+        System.out.println ("Edad" + " " + edad);
+        {
+            //Operadores  Aritmeticos
+            int valor1= 30;
+            int valor2= 20;
+            // Suma +
+            // Resta -
+            // Multiplicacion *
+            // Division /
+            int suma = valor1+valor2;
+            int resta = valor1-valor2;
+            int Multiplicacion = valor1*valor2;
+            int Division = valor1/valor2;
+
+            System.out.println("Suma" + suma);
+            System.out.println ( "Resta " + resta);
+            System.out.println  ( "Multiplicacion " + Multiplicacion);
+            System.out.println (" Division "+ Division);
+        }
+
+
+
+
+
+    }
+}
